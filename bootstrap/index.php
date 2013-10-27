0ae040afe251f2131222191075eb0f1c838c816e
Verena Thaenert <verena.thaenert@gmail.com>
2013-10-27 18:59:40 +0100

    Sieben

---
+++ bootstrap/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bootstrap 101 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="../../assets/js/html5shiv.js"></script>
+      <script src="../../assets/js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <h1>Abgabe</h1>
+    <h2>Web-Engineering</h2>
+    <table border="1">
+  <tr>
+    <th>Datum</th>
+    <th>Nummer</th>
+    <th>Name</th>
+    
+  </tr>
+  <tr>
+    <td>27.10.2013</td>
+    <td>Kurs 1</td>
+    <td>Verena Thaenert</td>
+    
+  </tr>
+  
+</table>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="//code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
